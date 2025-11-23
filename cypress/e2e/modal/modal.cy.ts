1a77de1a69265480ec12eb45cccc6add8500e5de
Ramis <you@example.com>
2025-11-23 11:34:36 +0300

    fix: fix cypress test for modal.

--- cypress/e2e/modal/modal.cy.ts
+++ cypress/e2e/modal/modal.cy.ts
@@ -19,21 +19,33 @@ describe('Тестирование модальных окон', () => {
     cy.wait(['@getIngredients', '@getUser', '@getOrders']);
   });
 
-  it('Должен открывать модальное окно ингредиента', () => {
-    cy.get('[data-cy="ingredient-bun"]').first().click();
+  it('Должен открывать модальное окно ингредиента и отображать правильный ингредиент', () => {
+    cy.get('[data-cy="ingredient-bun"]').first().as('clickedIngredient');
+
+    cy.get('@clickedIngredient')
+      .find('[class*="text_type_main"]')
+      .invoke('text')
+      .as('ingredientName');
+
+    cy.get('@clickedIngredient').click();
+
     cy.get('[data-cy="modal"]').should('be.visible');
+
+    cy.get('@ingredientName').then((name) => {
+      cy.get('[data-cy="modal"]').should('contain.text', name);
+    });
   });
 
   it('Должен закрывать модальное окно ингредиента при клике на кнопку закрытия', () => {
     cy.get('[data-cy="ingredient-bun"]').first().click();
-    cy.get('[data-cy="modal"]');
+    cy.get('[data-cy="modal"]').should('be.visible');
     cy.get('[data-cy="close-button"]').click();
     cy.get('[data-cy="modal"]').should('not.exist');
   });
 
   it('Должен закрывать модальное окно ингредиента при клике на оверлей', () => {
     cy.get('[data-cy="ingredient-bun"]').first().click();
-    cy.get('[data-cy="modal"]');
+    cy.get('[data-cy="modal"]').should('be.visible');
     cy.get('[data-cy="modal-overlay"]').click({ force: true });
     cy.get('[data-cy="modal"]').should('not.exist');
   });
